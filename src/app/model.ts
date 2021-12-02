6076433140b9b13f83e9c76964c2d60463bde5d2
Sinan Sarıçayır <sinansaricayir@gmail.com>
2021-12-02 18:24:31 +0300

    Final

---
+++ src/app/model.ts
@@ -0,0 +1,17 @@
+import { TodoItem } from "./todoitem";
+
+export class Model{
+    name:string;
+    items:TodoItem[];
+
+    constructor(){
+        this.name="Sinan";
+        this.items=[
+            {description:"kahvaltı",action:true},
+            {description:"spor",action:true},
+            {description:"alışveriş",action:false}
+            
+        ];
+
+    }
+}
---
+++ src/app/todoitem.ts
@@ -0,0 +1,13 @@
+// export class TodoItem{
+//     constructor(public description:string, public action:string){
+//         this.description=description;
+//         this.action=action;
+//     }
+// }
+
+export interface TodoItem{
+  
+    description:string;
+    action:boolean;
+
+}
